a6bebc638a31a1391379f74fdeb0661f8b8506f3
chongweiguan <75872136+chongweiguan@users.noreply.github.com>
2023-03-22 23:34:42 +0800

    initial commit

---
+++ src/utilities.js
@@ -0,0 +1,20 @@
+export const drawRect = (detections, ctx) => {
+    detections.forEach(prediction => {
+        //Get prediction results
+        const [x, y, width, height] = prediction['bbox'];
+        const text = prediction['class']
+
+        //Set styling properties
+        const color= 'green';
+        ctx.strokeStyle = color;
+        ctx.font = '18px Arial';
+        ctx.fillStyle = color;
+        ctx.lineWitdth = 10;
+
+        //Draw rectangle and text
+        ctx.beginPath();
+        ctx.fillText(text, x, y);
+        ctx.rect(x, y, width, height);
+        ctx.stroke();
+    })
+}
